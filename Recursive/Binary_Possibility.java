72b8b96b1aae8872e6bb614bf86a5510c9cca199
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2025-09-15 07:54:06 +0530

    Update Binary_Possibility.java

--- Recursive/Binary_Possibility.java
+++ Recursive/Binary_Possibility.java
@@ -5,8 +5,8 @@ public class  Binary_Possibility{
     public static void binary(String s,int num){
         if(num == 0) System.out.print(s+" ");
         else{
-            binary(s+'0', num-1);
-            binary(s+'1', num-1);
+           binary(s+'0', num-1); // possibility with 0
+            binary(s+'1', num-1); //possibilty with 1
         }
     }
     public static void main(String[] args) {
